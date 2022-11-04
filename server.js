940b4de6d94c622ee0a5c32cec8222d22e18e2eb
Aundrina Leyva <aundrinaleyva@Aundrinas-MacBook-Air.local>
2022-11-04 09:51:13 -0700

    test commit

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-const { client } = require("pg");
-const client = new client(
-  "postgres://localhost:5432/fitnessTrackerBackend-dev"
-);
-
-server.listen(PORT, () => {
-  console.log("The server is up on port", PORT);
-});
-
-module.exports = {
-  client,
-};
+console.log("express server");
