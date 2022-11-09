738c40bd5d139bc12eb1a80b050c6a8228604204
Aundrina Leyva <aundrinaleyva@Aundrinas-MacBook-Air.local>
2022-11-09 14:29:58 -0800

    path

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const routineRouter = require("./routines");
 const activitiesRouter = require("./activities");
+const raRouter = require("./routine_activities");
 
 router.get("/health", (req, res, next) => {
   res.send("All healthy and ready to go!");
@@ -10,5 +11,6 @@ router.get("/health", (req, res, next) => {
 router.use("/users", userRouter);
 router.use("/routines", routineRouter);
 router.use("/activities", activitiesRouter);
+router.use("/routine_activities", raRouter);
 
 module.exports = router;
